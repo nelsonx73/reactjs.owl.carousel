e0596b99331f3e5aab3bfc97535f03939c724a5e
Marmolejos <NelsonM@sandhillscenter.org>
2021-05-30 23:01:44 -0400

    first commit

---
+++ src/App/AppCarousel.jsx
@@ -0,0 +1,18 @@
+function AppCarousel() {
+  return (
+    <>
+      <div>Testing Carousel</div>
+      <div className="owl-carousel">
+        <div> Your Content </div>
+        <div> Your Content </div>
+        <div> Your Content </div>
+        <div> Your Content </div>
+        <div> Your Content </div>
+        <div> Your Content </div>
+        <div> Your Content </div>
+      </div>
+    </>
+  );
+}
+
+export default AppCarousel;
